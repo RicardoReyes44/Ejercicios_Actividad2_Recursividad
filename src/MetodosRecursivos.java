74e1929b7a2a61f993da0f1cac2cdd7862c7bd91
RicardoReyes44 <rickyboy1002@gmail.com>
2020-10-08 08:10:01 -0500

    Se agregaron los metodos 'factorial' y 'divisores' en la clase 'MetodosRecursivos'

--- src/MetodosRecursivos.java
+++ src/MetodosRecursivos.java
@@ -1,6 +1,23 @@
 
 public class MetodosRecursivos {
 	
+	public static long factorial(long num) {
+		if(num==1) {
+			return 1;
+		}else {
+			return num * factorial(num - 1);
+		}
+	}
 	
+    public static int divisores(int num, int div) {
+		if(div==1) {
+			return 1;
+		}else {
+			if(num%div==0) {
+				System.out.println(div);
+			}
+			return divisores(num, div-1);
+		}
+	}
 
 }
