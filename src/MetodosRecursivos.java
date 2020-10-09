3461b092cd82fd8d51565df092587472cb9d3809
RicardoReyes44 <rickyboy1002@gmail.com>
2020-10-09 00:57:38 -0500

    Se agregaron los metodos 'factorial' y 'divisores' en la clase 'MetodosRecursivos'

--- src/MetodosRecursivos.java
+++ src/MetodosRecursivos.java
@@ -20,4 +20,51 @@ public class MetodosRecursivos {
 		}
 	}
 
+    public static void cocientesEnteros(int menor,int mayor,int cam) {
+		if(cam>=menor) {
+			double cociente=mayor/cam;
+			if(mayor%cam==0) {
+				System.out.println("El cociente de " + mayor + "/" + cam + " es " + cociente);
+			}
+			cocientesEnteros(menor, mayor, cam-1);
+		}
+	}
+    
+    public static int divisoresEnteros(int menor,int mayor,int cam) {
+		if(cam>=menor) {
+			int mas=0;
+			if(mayor%cam==0) {
+				mas++;
+			}
+			return mas + divisoresEnteros(menor, mayor, cam-1);
+		}else {
+			return 0;
+		}
+	}
+    
+    public static void conversión() {
+		
+	}
+
+    public static int cantidadVocales(String cadena, int longitud, int vocal) {
+    	if(longitud>0) {
+    		int aux = (int)cadena.charAt(longitud);
+    		if(aux == 65 || aux == 69 || aux == 73 || aux == 79 || aux == 85) {
+    			vocal++;
+    		}
+    		return vocal + cantidadVocales(cadena, longitud-1, 0);
+    	}else{
+    		return 0;
+    	}
+    }
+    
+    public static void cadenaInversa(String cadena, int longitud) {
+		if(longitud==0) {
+			System.out.print(cadena.charAt(longitud));
+		}else{
+			System.out.print(cadena.charAt(longitud));
+			cadenaInversa(cadena, longitud-1);
+		}
+	}
+    
 }
--- src/PruebaEjercicio1.java
+++ src/PruebaEjercicio1.java
@@ -4,7 +4,17 @@ public class PruebaEjercicio1 {
 
 	public static void main(String[] args) {
 		
-        
+		public static void main(String[] args) {
+			
+	        Scanner entrada = new Scanner(System.in);
+			
+			System.out.println("Introduce numero: ");
+			int numero = entrada.nextInt();
+
+			System.out.println("\nDivisores");
+			System.out.println(MetodosRecursivos.divisores(numero, numero));
+
+		}
 	}
 
 }
