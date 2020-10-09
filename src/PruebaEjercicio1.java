2f6f8290745c96123879a6d06885e1ebc3f2b07c
RicardoReyes44 <rickyboy1002@gmail.com>
2020-10-09 01:55:33 -0500

    Se agregaron las pruebas a las clases de prueba 'PruebaEjercicio1' y 'PruebaEjercicio2'

--- src/PruebaEjercicio1.java
+++ src/PruebaEjercicio1.java
@@ -4,17 +4,14 @@ public class PruebaEjercicio1 {
 
 	public static void main(String[] args) {
 		
-		public static void main(String[] args) {
-			
-	        Scanner entrada = new Scanner(System.in);
-			
-			System.out.println("Introduce numero: ");
-			int numero = entrada.nextInt();
+        Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Introduce numero: ");
+		int numero = entrada.nextInt();
 
-			System.out.println("\nDivisores");
-			System.out.println(MetodosRecursivos.divisores(numero, numero));
+		System.out.println("\nDivisores");
+		System.out.println(MetodosRecursivos.divisores(numero, numero));
 
-		}
 	}
 
 }
--- src/PruebaEjercicio2.java
+++ src/PruebaEjercicio2.java
@@ -4,6 +4,13 @@ public class PruebaEjercicio2 {
 
 	public static void main(String[] args) {
 		
+        Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Introduce numero: ");
+		long numero = entrada.nextInt();
+		
+		System.out.println("\nFactorial");
+		System.out.println(MetodosRecursivos.factorial(numero));
       
 	}
 
