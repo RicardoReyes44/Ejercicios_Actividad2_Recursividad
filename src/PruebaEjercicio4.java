74aa1be89859967f64074bfd0f30b6a3b00cbf47
RicardoReyes44 <rickyboy1002@gmail.com>
2020-10-09 01:58:57 -0500

    Se agregaron las pruebas a las clases de prueba 'PruebaEjercicio3' y 'PruebaEjercicio4'

--- src/PruebaEjercicio4.java
+++ src/PruebaEjercicio4.java
@@ -4,7 +4,15 @@ public class PruebaEjercicio4 {
 
 	public static void main(String[] args) {
 		
-        
+        Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Introduce numer mayor: ");
+		int mayor = entrada.nextInt();
+		System.out.println("Introduce numer menor: ");
+		int menor = entrada.nextInt();
+		
+		System.out.println("\nDivisores enteros");
+		System.out.println(MetodosRecursivos.divisoresEnteros(menor, mayor, mayor));
 
 	}
 
