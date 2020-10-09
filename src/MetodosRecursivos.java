eeb12ac23580ad58ab401d58cacc5034d68598e6
RicardoReyes44 <rickyboy1002@gmail.com>
2020-10-09 01:23:53 -0500

    Se agregaron los metodos de 'cocientesEnteros' y 'divisoresEnteros' en la clase 'MetodosRecursivos'

--- src/MetodosRecursivos.java
+++ src/MetodosRecursivos.java
@@ -18,8 +18,8 @@ public class MetodosRecursivos {
 			}
 			return divisores(num, div-1);
 		}
-	}
-
+    }
+    
     public static void cocientesEnteros(int menor,int mayor,int cam) {
 		if(cam>=menor) {
 			double cociente=mayor/cam;
@@ -39,31 +39,6 @@ public class MetodosRecursivos {
 			return mas + divisoresEnteros(menor, mayor, cam-1);
 		}else {
 			return 0;
-		}
-	}
-    
-    public static void conversión() {
-		
-	}
-
-    public static int cantidadVocales(String cadena, int longitud, int vocal) {
-    	if(longitud>0) {
-    		int aux = (int)cadena.charAt(longitud);
-    		if(aux == 65 || aux == 69 || aux == 73 || aux == 79 || aux == 85) {
-    			vocal++;
-    		}
-    		return vocal + cantidadVocales(cadena, longitud-1, 0);
-    	}else{
-    		return 0;
-    	}
-    }
-    
-    public static void cadenaInversa(String cadena, int longitud) {
-		if(longitud==0) {
-			System.out.print(cadena.charAt(longitud));
-		}else{
-			System.out.print(cadena.charAt(longitud));
-			cadenaInversa(cadena, longitud-1);
 		}
 	}
     
